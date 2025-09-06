760a4096755f306df83a35bbad3623507ea2ffc1
“Perrier” <perrierbottle.eaupetillante@gmail.com>
2025-09-06 22:35:22 +0200

    I want to break things

--- server/index.js
+++ server/index.js
@@ -40,8 +40,8 @@ app.use(helmet());
 app.use(cors({
   origin: '*',
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept']
+  methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
+  allowedHeaders: 'Authorization,Content-Type'
 }));
 
 // Rate limiting - More permissive for navigation and authenticated users
